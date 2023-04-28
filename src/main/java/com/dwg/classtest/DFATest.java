36d7358f6fe9fd268f7e86d6970caaab3a1f8cad
Autrui <1719910793@qq.com>
2023-04-28 11:50:17 +0800

    sync&DFA

---
+++ src/main/java/com/dwg/classtest/DFATest.java
@@ -0,0 +1,18 @@
+package com.dwg.classtest;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @author Autrui
+ * @date 2023/4/28
+ * @apiNote
+ */
+public class DFATest {
+    @SuppressWarnings({})
+    public void addSensitiveWordToHashList(Set<String> keyWordSet) {
+        HashMap sensitiveWordMap = new HashMap(keyWordSet.size());
+
+    }
+}
